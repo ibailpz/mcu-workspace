37e625c4aaed11c0e27748b153c12ec71c0ca68e
ibi13x@gmail.com <ibi13x@gmail.com@9cdaa720-f943-c147-bbf6-84d066f78222>
2013-12-23 12:52:08 +0000

    Added: 	- Widget: static information
    TODO:
    	- Spinner: load the adapter with arrParkings or arrFavoriteParkings,
    depending the value selected
    	- ParkingList menu: open map with all the parkings in the list and
    filter the list with the parkings near the users position
    	- ParkingInfo: insert a map with its position and add a check box to
    activate notifications
    	- Service
    	- JSON
    	- BD

--- P1-JustPark/src/es/deusto/p1justpark/widget/WidgetProvider.java
+++ P1-JustPark/src/es/deusto/p1justpark/widget/WidgetProvider.java
@@ -1,16 +1,40 @@
 package es.deusto.p1justpark.widget;
 
+import java.util.ArrayList;
+
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.widget.RemoteViews;
+import es.deusto.p1justpark.R;
+import es.deusto.p1justpark.data.Parking;
 
-public class WidgetProvider extends AppWidgetProvider{
-	
+public class WidgetProvider extends AppWidgetProvider {
+
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
-		
-		super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+		// We must iterate all the widget instances
+		for (int i = 0; i < appWidgetIds.length; i++) {
+			int widgetId = appWidgetIds[i];
+
+			ArrayList<Parking> favoriteParkings = new ArrayList<Parking>();
+			favoriteParkings.add(new Parking(1, "Parking Plaza Euskadi",
+					"Plaza Euskadi Bilbao", "100", 43.26723, -2.93839));
+
+			RemoteViews views = new RemoteViews(context.getPackageName(),
+					R.layout.widget_layout);
+			Parking parking = favoriteParkings.get(0);
+			if (parking != null) {
+				views.setTextViewText(R.id.widget_parking_name,
+						parking.getName());
+				views.setTextViewText(R.id.widget_parking_places, parking
+						.getPlaces().toString());
+			}
+
+			appWidgetManager.updateAppWidget(widgetId, views);
+		}
 	}
 
 }
