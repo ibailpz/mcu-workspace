6b51690a48021574835ad3ffb34d39a7cf66bc3a
hasier.rodriguez@opendeusto.es <hasier.rodriguez@opendeusto.es@9cdaa720-f943-c147-bbf6-84d066f78222>
2013-12-24 09:40:55 +0000

    Fix CAB bug, wrong list

--- P1-JustPark/src/es/deusto/p1justpark/ui/AdapterObserver.java
+++ P1-JustPark/src/es/deusto/p1justpark/ui/AdapterObserver.java
@@ -1,11 +1,10 @@
 package es.deusto.p1justpark.ui;
 
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import es.deusto.p1justpark.data.Parking;
 
 public interface AdapterObserver {
 
-	public void onAdapterChanged(ArrayAdapter<Parking> adapter, ListView list);
+	public void onAdapterChanged(ArrayAdapter<Parking> adapter);
 
 }
