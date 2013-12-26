fc8e4f1f712ed2f2ef181c0a852fb0e3cdfe6b38
ibi13x@gmail.com <ibi13x@gmail.com@9cdaa720-f943-c147-bbf6-84d066f78222>
2013-12-26 16:46:35 +0000

    Removed notifications interval.

--- P1-JustPark/src/es/deusto/p1justpark/settings/SettingsFragment.java
+++ P1-JustPark/src/es/deusto/p1justpark/settings/SettingsFragment.java
@@ -21,35 +21,18 @@ public class SettingsFragment extends PreferenceFragment implements
 
 		String[] array = getResources()
 				.getStringArray(R.array.general_interval);
-		findPreference("notificacions_interval").setSummary(
-				array[Integer.parseInt(PreferenceManager
-						.getDefaultSharedPreferences(getActivity()).getString(
-								"notificacions_interval", "0"))]);
 
-		array = getResources().getStringArray(R.array.notificacions_interval);
 		findPreference("general_interval").setSummary(
 				array[Integer.parseInt(PreferenceManager
 						.getDefaultSharedPreferences(getActivity()).getString(
 								"general_interval", "0"))]);
 
-		findPreference("notificacions_interval").setEnabled(
-				PreferenceManager.getDefaultSharedPreferences(getActivity())
-						.getBoolean("switch_notifications", true));
 	}
 
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
 			String key) {
-		if (key.equals("switch_notifications")) {
-			findPreference("notificacions_interval").setEnabled(
-					sharedPreferences.getBoolean(key, true));
-		} else if (key.equals("notificacions_interval")) {
-			String[] array = getResources().getStringArray(
-					R.array.notificacions_interval);
-			findPreference(key).setSummary(
-					array[Integer.parseInt(sharedPreferences
-							.getString(key, "0"))]);
-		} else if (key.equals("general_interval")) {
+		if (key.equals("general_interval")) {
 			String[] array = getResources().getStringArray(
 					R.array.general_interval);
 			findPreference(key).setSummary(
