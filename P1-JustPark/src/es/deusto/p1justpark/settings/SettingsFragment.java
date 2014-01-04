888b1aee8cf2417402b0f4f82eb660d448b61a3d
hasier.rodriguez@opendeusto.es <hasier.rodriguez@opendeusto.es@9cdaa720-f943-c147-bbf6-84d066f78222>
2014-01-04 11:03:43 +0000

    Fix SettingsFragment not attached on change

--- P1-JustPark/src/es/deusto/p1justpark/settings/SettingsFragment.java
+++ P1-JustPark/src/es/deusto/p1justpark/settings/SettingsFragment.java
@@ -54,30 +54,35 @@ public class SettingsFragment extends PreferenceFragment implements
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
 			String key) {
-		if (key.equals(getResources().getString(R.string.general_interval_key))) {
-			String[] array = getResources().getStringArray(
-					R.array.general_interval);
-			findPreference(key).setSummary(
-					array[Integer.parseInt(sharedPreferences
-							.getString(key, "0"))]);
-		} else if (key.equals(getResources().getString(
-				R.string.automatic_update_key))) {
-			if (sharedPreferences.getBoolean(key, true)) {
-				findPreference(
-						getResources().getString(R.string.general_interval_key))
-						.setEnabled(true);
-				findPreference(
-						getResources().getString(
-								R.string.switch_notifications_key)).setEnabled(
-						true);
-			} else {
-				findPreference(
-						getResources().getString(R.string.general_interval_key))
-						.setEnabled(false);
-				findPreference(
-						getResources().getString(
-								R.string.switch_notifications_key)).setEnabled(
-						false);
+		if (isAdded()) {
+			if (key.equals(getResources().getString(
+					R.string.general_interval_key))) {
+				String[] array = getResources().getStringArray(
+						R.array.general_interval);
+				findPreference(key).setSummary(
+						array[Integer.parseInt(sharedPreferences.getString(key,
+								"0"))]);
+			} else if (key.equals(getResources().getString(
+					R.string.automatic_update_key))) {
+				if (sharedPreferences.getBoolean(key, true)) {
+					findPreference(
+							getResources().getString(
+									R.string.general_interval_key)).setEnabled(
+							true);
+					findPreference(
+							getResources().getString(
+									R.string.switch_notifications_key))
+							.setEnabled(true);
+				} else {
+					findPreference(
+							getResources().getString(
+									R.string.general_interval_key)).setEnabled(
+							false);
+					findPreference(
+							getResources().getString(
+									R.string.switch_notifications_key))
+							.setEnabled(false);
+				}
 			}
 		}
 	}
